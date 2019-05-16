af1ea6d64a7f38aa12a2eb1a09f7e4ec240c3276
Aziz Ali <aziz@ilovecoding.org>
2019-05-16 16:22:26 -0700

    More about state, immutability and immer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Score from './Score'
 import Tracker from './Tracker'
+import produce from 'immer'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      list: [{ name: 'T' }, { name: 'L' }],
       input: 'input',
       input2: 'input2',
       counter: 0,
@@ -19,26 +21,62 @@ export default class App extends React.Component {
     const value = event.target.value
     console.log(value)
     // this.state.input = value
+    // Way 1: First Param is Object
     this.setState({
       input: value
     })
   }
 
   handleClick = () => {
-    this.setState({
-      counter: this.state.counter + 1
+    // // Way 1: First Param is Object
+    // this.setState({
+    //   counter: this.state.counter + 1
+    // }, () => {
+    //   console.log(this.state.counter)
+    // })
+
+    const newState = produce(this.state, (draft) => {
+      draft.list[0].name = 'new name'
     })
+
+    // this.setState({
+    //   ...newState
+    // })
+
+    this.setState(() => newState)
   }
 
   handleLogout = () => {
-    this.setState({
-      isLoggedIn: false
+    // this.setState({
+    //   isLoggedIn: false
+    // })
+    console.log('Before')
+    // Way 2: First Param is Function
+    this.setState((state, props) => {
+      console.log('inside')
+      if (state.counter < 2) {
+        return {
+          ...state,
+          isLoggedIn: false
+        }
+      } else {
+        return state
+      }
+    }, () => {
+      console.log(this.state.counter)
     })
+    console.log('after')
   }
 
   render() {
     return (
-      <div style={{ padding: '10px', border: '2px solid black' }}>
+      <div style={{
+        padding: '10px',
+        border: '2px solid black'
+      }}>
+        <pre>
+          {JSON.stringify(this.state, null, 2)}
+        </pre>
         Hello world Class {this.props.name}
         {this.state.isLoggedIn ? 'I am logged In' : 'I am logged out'}
         <input
